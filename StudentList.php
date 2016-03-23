b6571133bb2d49ee9ddaba829840487c892a1533
FightingGinger <carsonhall11@apu.edu>
2016-03-22 17:05:07 -0700

    This is the Advisers student search
    Pushed StudentID through

---
+++ StudentList.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<?php 
+
+	$fulldb = mysql_connect('localhost', 'root', '');
+
+	mysql_select_db('aahelper', $fulldb);
+
+	$students = "SELECT fname, lname, studentid 
+					FROM student";
+
+	$result = mysql_query($students);
+
+	$numRows = mysql_num_rows($result); 
+	echo "<h1>" . $numRows . " Student" . ($numRows == 1 ? "" : "s") . " Available </h1>"; 
+
+	//display all students so advisor can pick a student and then
+	// their studentid will be passed on
+	?>
+	<form action = "StudentList2.php" method = "post">
+	<table style="width=300">
+	<?php
+	while($row = mysql_fetch_array($result))
+	{
+	?>
+	<tr>
+		<td>
+		  <?php echo $row["studentid"] ?>
+		  <input type="radio" name="studentnum" value="<?php echo $row["studentid"] ?>" ><br>
+		</td>
+		<td>
+			<?php
+			  echo $row["fname"] . " " . $row["lname"];
+			?>
+		</td>
+	</tr>
+	<?php
+	}
+
+	mysql_close($fulldb);
+	?>
+	</table>
+	<input type="submit" name="submit" value="Submit"> 
+	</form>
+
+</body>
+</html>
