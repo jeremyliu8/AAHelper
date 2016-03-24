785d1aa60d08fb142534898e6656b9cfcf52e882
FightingGinger <carsonhall11@apu.edu>
2016-03-24 01:11:35 -0700

    updated with form format

--- StudentList2.php
+++ StudentList2.php
@@ -1,22 +1,123 @@
-Hello there 
+<link rel="stylesheet" href="css/bootstrap.css">
 <?php
 // studentid of selected student
-echo $_POST["studentnum"]; 
+$stunum = $_SESSION["studentid"];
 
 //open database
 $fulldb = mysql_connect('localhost', 'root', '');
 
 mysql_select_db('aahelper', $fulldb);
 
+$studentinfo = "SELECT * FROM student where studentid='$stunum'";
 
-//course history from CS student
-$students = "SELECT * FROM csstudentcourse";
+$studentstuff = mysql_query($studentinfo);
+
+while ($row = mysql_fetch_object($studentstuff)) {
+?>
+<nav class="navbar navbar-default navbar-inverse">
+  <div class="container-fluid"> 
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand">
+      <?php 
+      $studentmajor = $row->major;
+      if($studentmajor == "CS"){
+      	echo "Computer Science";
+      }
+      elseif($studentmajor == "CIS"){
+      	echo "Computer Information Systems";
+      }
+       ?>
+      </a></div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        
+        <li> </li>
+        <li> </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a>
+        <?php	echo $row->fname;
+        		echo " ";
+				echo $row->lname; ?>
+        </a></li>
+        <li>&nbsp;</li>
+        <li><a><?php echo $row->studentid; ?></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<?php
+}
 
 
 
-$result = mysql_query($students);
+//course history from studentcourse
 
+$coursehistory = "SELECT * FROM studentcourse where studentid='$stunum'";
 
+$result = mysql_query($coursehistory);
+
+//magic
+
+?> 
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="jumbotron">
+  		<div class="panel panel-default">
+    <table class="table table-condensed table-bordered">
+  	<tr>
+	    <th>Course ID</th>
+	    <th>Course Grade</th>
+	    <th>Term Taken</th>
+	    <th>Year Taken</th>
+  	</tr>
+<?php
+
+while ($row = mysql_fetch_object($result)) {
+	?> <tr> <td> <?php
+	echo $row->courseid;
+	?> </td> <?php
+	?> <td> <?php
+	echo $row->grade;
+	?> </td> <?php
+	$tt = $row->termtaken;
+	$ttyear = substr($tt, 0, 4);
+	$ttterm = substr($tt, 4);
+	?> <td> <?php
+	if($ttterm == 1){
+		echo "Spring";
+	}
+	elseif($ttterm == 3){
+		echo "Summer";
+	}
+	elseif($ttterm == 7){
+		echo "Fall";
+	}
+	?> </td> <?php
+	?> <td> <?php
+	echo $ttyear;
+	?> </td> <?php
+}
+
+?> </tr>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div> <?php
 
 mysql_close($fulldb);
 ?>
+
+<footer class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <p>&copy; 2015 Finger Puppet Mafia <br/ > Azusa Pacific University <br/ ></p>
+      </div>
+    </div>
+  </div>
+</footer>
