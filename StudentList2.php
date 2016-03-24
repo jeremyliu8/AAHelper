032f0ed822cb153e6723248a31eaa1677e1c3183
Jeremy Liu <jeremykliu8@gmail.com>
2016-03-24 01:21:41 -0700

    Clean Up

--- StudentList2.php
+++ StudentList2.php
@@ -1,7 +1,16 @@
 Hello there 
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
 <?php
 // studentid of selected student
-echo $_POST["studentnum"]; 
+// $stunum = $_POST["studentnum"];
+// echo $_POST["studentnum"]; 
+
+$stunum = '002406078';
+echo $stunum;
 
 //open database
 $fulldb = mysql_connect('localhost', 'root', '');
@@ -9,14 +18,45 @@ $fulldb = mysql_connect('localhost', 'root', '');
 mysql_select_db('aahelper', $fulldb);
 
 
-//course history from CS student
-$students = "SELECT * FROM csstudentcourse";
+//course history from studentcourse
 
+$coursehistory = "SELECT * FROM studentcourse where studentid='$stunum'";
 
+$result = mysql_query($coursehistory);
 
-$result = mysql_query($students);
+//magic
 
+?> 
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="jumbotron">
+    <table>
+  	<tr>
+	    <th>Course ID</th>
+	    <th>Course Grade</th>
+	    <th>Term Taken</th>
+  	</tr>
+<?php
 
+while ($row = mysql_fetch_object($result)) {
+	?> <tr> <td> <?php
+	echo $row->courseid;
+	?> </td> <?php
+	?> <td> <?php
+	echo $row->grade;
+	?> </td> <?php
+	?> <td> <?php
+	echo $row->termtaken;
+	?> </td> <?php
+}
+
+?> </tr>
+</table>
+</div>
+</div>
+</div>
+</div> <?php
 
 mysql_close($fulldb);
 ?>
