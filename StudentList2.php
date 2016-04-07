02461f74f689773691db084146fa24c925b777d5
FightingGinger <carsonhall11@apu.edu>
2016-04-07 00:45:48 -0700

    updated looks funky fresh

--- StudentList2.php
+++ StudentList2.php
@@ -1,117 +1,131 @@
-<link rel="stylesheet" href="css/bootstrap.css">
-<?php
-    session_start();
-// studentid of selected student
-$stunum = $_SESSION["studentid"];
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<?php 
+	session_start();
 
-//open database
-require_once 'includes/db_connect.php';
-require_once 'includes/functions.php';
+	//open database
+	require_once 'includes/db_connect.php';
+	require_once 'includes/functions.php';
 
-if (!logged_in()) {
-  header("Location: index.php");
-}
+	if (!logged_in()) {
+  		header("Location: index.php");
+	}
 
-$studentinfo = "SELECT * FROM student where studentid='$stunum'";
+	write_to_file($_SESSION, "Session Variables");
 
-$studentstuff = $connection->query($studentinfo);
+	$studentMajor = $_SESSION['major'];
+	if ($studentMajor == 'CS') {
+		$studentMajor = "Computer Science";
+	}
+	elseif ($studentMajor == 'CIS') {
+		$studentMajor = "Computer Information Systems";
+	}
 
-while ($row = $studentstuff->fetch_array()) {
+	write_to_file($studentMajor, "\$studentMajor");
+
 ?>
 <nav class="navbar navbar-default navbar-inverse">
   <div class="container-fluid"> 
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-      <a class="navbar-brand">
-      <?php 
-      $studentmajor = $row['major'];
-      if($studentmajor == "CS"){
-      	echo "Computer Science";
-      }
-      elseif($studentmajor == "CIS"){
-      	echo "Computer Information Systems";
-      }
-       ?>
-      </a></div>
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        
-        <li> </li>
-        <li> </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a>
-        <?php	echo $row['fname'];
-        		echo " ";
-				echo $row['lname']; ?>
-        </a></li>
-        <li>&nbsp;</li>
-        <li><a><?php echo $row['studentid']; ?></a></li>
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"></button>
+      <a class="navbar-brand"> <?php echo $studentMajor; ?> </a>
+    </div>
+	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	    <ul class="nav navbar-nav">
+          <li> </li>
+          <li> </li>
+	    </ul>
+	    <ul class="nav navbar-nav navbar-right">
+	      <li><a>
+	        <?php	
+	        	echo $_SESSION['fname'];
+	        	echo " ";
+				echo $_SESSION['lname']; 
+			?>
+	      </a></li>
+	      <li>&nbsp;</li>
+        <li><a><?php echo $_SESSION['studentid']; ?></a></li>
       </ul>
     </div>
   </div>
 </nav>
-<?php
-}
 
-
-
-//course history from studentcourse
-
-$coursehistory = "SELECT * FROM studentcourse where studentid='$stunum'";
-
-$result = $connection->query($coursehistory);
-
-//magic
-
-?> 
+<body background="img/sunset.jpg">
+<!-- the table -->
 <div class="container">
   <div class="row">
     <div class="col-lg-12">
       <div class="jumbotron">
-  		<div class="panel panel-default">
-    <table class="table table-condensed table-bordered">
-  	<tr>
-	    <th>Course ID</th>
-	    <th>Course Grade</th>
-	    <th>Term Taken</th>
-	    <th>Year Taken</th>
-  	</tr>
-<?php
+	  		<div class="panel panel-default">
+			    <table class="table table-condensed table-bordered">
+			    <tr>
+			    <?php $startyear = $_SESSION['startyear']; ?>
+				    <th colspan="3"></th>
+				    <?php for($x = 0; $x < 5; $x++){ ?>
+			    	<th colspan="3"><center><?php echo $startyear + $x ?></center></th>
+			    	<?php } ?>
+				    <th></th>
+				</tr>
 
-while ($row = $result->fetch_array()) {
-	?> <tr> <td> <?php
-	echo $row['courseid'];
-	?> </td> <?php
-	?> <td> <?php
-	echo $row['grade'];
-	?> </td> <?php
-	$termAndYear = $row['termtaken'];
-	$yearTaken = substr($termAndYear, 0, 4);
-	$termTaken = substr($termAndYear, 4);
-	?> <td> <?php
-	if($termTaken == 1){
-		echo "Spring";
-	}
-	elseif($termTaken == 4){
-		echo "Summer";
-	}
-	elseif($termTaken == 7){
-		echo "Fall";
-	}
-	?> </td> <?php
-	?> <td> <?php
-	echo $yearTaken;
-	?> </td> <?php
-}
 
-?> </tr>
-</table>
-</div>
-<center><a href="includes/logout.php">Logout</a></center>
-</div>
-</div>
-</div>
+				  	<tr>
+					    <th>Class Name</th>
+					    <th>Course ID</th>
+					    <th>units</th>
+					    <?php for($x = 0; $x < 5; $x++){ ?>
+						    <th>F</th>
+						    <th>S</th>
+						    <th><i class="fa fa-sun-o"></i></th>
+					    <?php } ?>
+					    <th>Course Grade</th>
+				  	</tr>
+
+					<?php
+
+					$sql_coursemajor = "SELECT * 
+						FROM courses JOIN major 
+						ON courses.courseid = major.courseid 
+						WHERE majorid = 'CS';";
+
+					$sql_coursemajor = $connection->query($sql_coursemajor);
+					//takes every courseid with correct major
+					while ($row = $sql_coursemajor->fetch_array()) {
+						// $currentCourse = $row['courseid'];
+						// echo $currentCourse;
+						?>
+						<tr> 
+							<td> <?php echo $row['classname']; ?> </td>
+							<td> <?php echo $row['courseid']; ?> </td> 
+							<td> <?php echo $row['units']; ?> </td> 
+							<?php
+								//break up terms
+								$termnum = $row['term'];
+								$fall = substr($termnum, 0,1);
+								$spring = substr($termnum, 1,1);
+								$summer = substr($termnum, 2,1);
+							
+							//replicate for all 5 years
+							for($x = 0; $x < 5; $x++){ ?>
+							<td <?php echo validate_term($fall); ?> ></td>
+							<td <?php echo validate_term($spring); ?> ></td>
+							<td <?php echo validate_term($summer); ?> ></td>
+							<?php } ?>
+							<!-- end replication -->
+
+							<td> <?php echo "A"; ?> </td> 
+						</tr> 
+						<?php
+					} ?>
+					</table>
+				</div>
+				<center><a href="includes/logout.php">Logout</a></center>
+	  	</div>
+		</div>
+  </div>
 </div> 
 
 <?php
@@ -122,3 +136,5 @@ while ($row = $result->fetch_array()) {
 <center>
   <?php include 'footer.php'; ?>
 </center>
+</body>
+</html>
