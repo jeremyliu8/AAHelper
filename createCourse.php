0bc992c5fba23b42199d25654ef680aecfb44540
csissoyev8 <csissoyev11@apu.edu>
2016-05-05 02:01:43 -0700

    fixed create course

--- createCourse.php
+++ createCourse.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New User</title>
-    <link rel="stylesheet" type="text/css" href="css/main.css"/>
-    <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,700' rel='stylesheet' type='text/css'>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-select.css"/>
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+        <title>Add a Course</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- // <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script> -->
+        <script src="js/bootstrap-select.js"/></script>
+        <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
@@ -46,18 +51,41 @@
              VALUES ('$courseid', '$classname', '$units', '$term')";
     
     if ($connection->query($newCourse) === TRUE) {
-        echo "<h2 class='success'>&#x2713; New Class Added Successfully!</h2>";
-        echo "<p>Click <a href='advisor_home.php'>here</a> to go back</p>";
-    } else {
-        echo "<h2 class='error'>&#x2717; Uh-oh! There was an error adding the class!</h2>";
-        echo "<p>Error: " . $connection->error . "</p>";
-        echo "<p>Click <a href='add_class.php'>here</a> to go try again</p>";
-    }
+        ?>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <h2 class='success'>&#x2713; New Class Added Successfully!</h2>
+                        <p>Click <a href='advisor_home.php'>here</a> to go back</p>
+                    </div>
+                    <div class="col-md-4">
+                    </div>
+                </div>  
+            </div>
+    <?php } else { ?>
+            <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                        </div>
+                        <div class="col-md-4 text-center">
+                            <h2 class='error'>&#x2717; Uh-oh! There was an error adding the class!</h2>
+                            <p>Error: <?php echo $connection->error ?> </p>
+                            <p>Click <a href='add_class.php'>here</a> to go try again</p>
+                        </div>
+                        <div class="col-md-4">
+                        </div>
+                    </div>  
+                </div>
+    <?php }
 
 
     $connection->close();
     ?>
     <!-- include footer -->
-    <?php include 'footer.php'; ?>
+      <div class="row text-center extra-top">
+            <?php include_once "footer.php" ?>
+      </div> 
 </body>
 </html>
